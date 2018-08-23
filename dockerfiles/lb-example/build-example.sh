b95951b7572b252693bd59d832febad03c6dfdfc
virtimus <virtimus@gmail.com>
2018-08-24 00:04:07 +0200

    Update build-example.sh

--- dockerfiles/lb-example/build-example.sh
+++ dockerfiles/lb-example/build-example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /i3c/data
-git clone https://github.com/strongloop-community/loopback-example-connector.git -b soap
-cd loopback-example-connector
+git clone https://github.com/virtimus/i3c-loopback-example-connector.git -b soap
+cd i3c-loopback-example-connector
 #apk add make g++ not a must i think
 echo $(pwd)
 npm install --no-bin-links
